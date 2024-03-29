535a7d0e4f291e9538b227110660a33cf1e0fb4f
GiorgiSX <165401863+GiorgiSX@users.noreply.github.com>
2024-03-29 19:04:01 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var myString = "This is a string";
+
+
+let myNumber = 42;
+
+
+const concatenationResult = "Number: " + myNumber;
+
+
+const name = "George";
+const age = 20;
+const message = "Hello, my name is " + "George" + " and I am " + "20" + " years old.";
+
+console.log(myString);
+console.log(myNumber);
+console.log(concatenationResult);
+console.log(message);
